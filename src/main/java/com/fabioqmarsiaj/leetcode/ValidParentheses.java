725ff711596ec6fb38c405651307e3f8a5f11e4a
fabioqmarsiaj <fabiobackend@gmail.com>
2023-03-27 16:42:51 -0300

    Valid Parentheses challange

---
+++ src/main/java/com/fabioqmarsiaj/leetcode/ValidParentheses.java
@@ -0,0 +1,26 @@
+package com.fabioqmarsiaj.leetcode;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        isValid("[(}]");
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}

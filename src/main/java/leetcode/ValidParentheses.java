abede990e38eba8214e18c7175f2d52f7a869aaa
fabioqmarsiaj <fabiobackend@gmail.com>
2023-03-27 22:01:59 -0300

    Explanation for ValidParentheses

--- src/main/java/leetcode/ValidParentheses.java
+++ src/main/java/leetcode/ValidParentheses.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public static void main(String[] args) {
-        isValid("[(}]");
+        isValid("[()]");
     }
 
     public static boolean isValid(String s) {
@@ -18,6 +18,9 @@ public class ValidParentheses {
                 stack.push('}');
             else if (c == '[')
                 stack.push(']');
+            // ! The stack.pop removes the last inserted character and compares with the current 'c'
+            // ! stack.pop = ')' != '}' is TRUE, therefore returns false
+            // * stack.pop = ')' != ')' is FALSE, therefore it does not return until stack.isEmpty()
             else if (stack.isEmpty() || stack.pop() != c)
                 return false;
         }
